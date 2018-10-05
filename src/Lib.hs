788434c01afd4b57f4e1bd635d8fb217d833fa35
ocramz <zocca.marco@gmail.com>
2018-10-05 16:15:56 +0200

    first

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,19 @@
 module Lib where
 
+-- data Expr a =
+--     Const a
+--   | Var String
+
+type Var = String
+
+data Expr a =
+    Const a
+  | Let Var (Expr a)
+  deriving (Eq, Show)
+
+
+eval e = case e of
+  Const x -> x
+  
+
 
