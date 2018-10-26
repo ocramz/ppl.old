ab2f408640651d0422c2fbd7ad4168089791a65a
ocramz <zocca.marco@gmail.com>
2018-10-26 17:50:36 +0200

    fixed Wadler92.ex0

--- src/PPL/Internal/Wadler92.hs
+++ src/PPL/Internal/Wadler92.hs
@@ -31,18 +31,6 @@ instance Show a => Show (Value m a) where
 -- data Op2 a = Plus a a | Minus a a | Times a a deriving (Eq, Show)
 
 
--- ex0 = App (Lam ()) (Const 41)
-
-ex0 :: Expr IM.Key Int
--- ex0 = App (Plus (Var 0) (Var 0)) $ Plus (Const 10) (Const 11)
-
-ex0 = Plus (Const 2) (Const 3)
-
-test :: Expr IM.Key Int -> Maybe (Value Maybe Int)
-test = interp empty
-
-
-
 add :: Num a => Value m1 a -> Value m2 a -> Maybe (Value m3 a)
 add = liftOp2 (+)
 
@@ -94,3 +82,14 @@ augment x (Env mm) = Env $ IM.insert k x mm where
 
 lookup :: IM.Key -> Env a -> Maybe a
 lookup v (Env mm) = IM.lookup v mm
+
+
+
+
+-- λ> test ex0
+-- Just 42
+ex0 :: Expr IM.Key Int
+ex0 = App (Lam (Plus (Var 0) (Var 0))) $ Plus (Const 10) (Const 11)
+
+test :: Expr IM.Key Int -> Maybe (Value Maybe Int)
+test = interp empty
