f93f2397191dd9ee9fe79c0def729632c72bac30
ocramz <zocca.marco@gmail.com>
2018-10-28 14:49:06 +0100

    Internal.Mini : add exceptions

--- src/PPL/Internal/Mini.hs
+++ src/PPL/Internal/Mini.hs
@@ -1,10 +1,11 @@
 {-# language LambdaCase #-}
 module PPL.Internal.Mini where
 
--- | Value type
-data Value a =
-    Val a
-  | Fun (Value a -> Value a)
+import Control.Monad.Catch (MonadThrow(..), throwM)
+import GHC.Exception
+import Data.Typeable
+
+
 
 -- | Primitive operations
 data OP = Plus | Times deriving (Eq, Show)
@@ -24,7 +25,24 @@ let_ v e body = (v :-> body ) :$ e
 lets_ :: Foldable t => t (v, Expr v a) -> Expr v a -> Expr v a
 lets_ decls body = foldl (flip $ uncurry let_) body decls
 
+
+-- | Value type
+data Value a =
+    Val a
+  | Fun (Value a -> Value a)
+
 -- | Evaluating an 'Expr' produces a 'Value'
 eval = \case
   Const x -> Val x
   v :-> body -> undefined
+  where
+    apply v x = case v of
+      Fun f -> f x
+
+
+
+
+-- * Exceptions
+
+data Except = NotAFunction String deriving (Eq, Show, Typeable)
+instance Exception Except
