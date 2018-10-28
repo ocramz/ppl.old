33e3a4e1ef9839dcae35059c271486bd55558c5c
ocramz <zocca.marco@gmail.com>
2018-10-28 23:23:27 +0100

    refactoring Internal.Mini

--- src/PPL/Internal/Mini.hs
+++ src/PPL/Internal/Mini.hs
@@ -6,42 +6,63 @@ import GHC.Exception
 import Data.Typeable
 
 
-
--- | Primitive operations
-data OP = Plus | Times deriving (Eq, Show)
-
--- | Expression type
-data Expr v a =
-    v :-> Expr v a       -- ^ Lambda
-  | Expr v a :$ Expr v a -- ^ Apply
-  -- | Prim OP (Expr v a) (Expr v a) -- ^ Primitive operation (e.g. '+')
-  | Const a
+data V i =
+    B i  -- ^ Bound variables
+  | F i  -- ^ Free variables
   deriving (Eq, Show)
 
--- | 'Let' is equivalent to applying a lambda
-let_ :: v -> Expr v a -> Expr v a -> Expr v a
-let_ v e body = (v :-> body ) :$ e
+mkVar :: V Integer
+mkVar = F 0
 
-lets_ :: Foldable t => t (v, Expr v a) -> Expr v a -> Expr v a
-lets_ decls body = foldl (flip $ uncurry let_) body decls
+data Expr i a =
+    Var (V i) -- ^ Variable
+  | Lam (Expr i a)  -- ^ Lambda
+  | Expr i a :$ Expr i a  -- ^ Application
+  deriving (Eq, Show)
 
 
--- | Value type
-data Value m a =
-    Val a
-  | Fun (Value m a -> m (Value m a))
 
--- | Evaluating an 'Expr' produces a 'Value'
-eval env = \case
-  Const x -> pure $ Val x
-  v :-> body -> pure $ Fun $ \v -> eval env' body  -- implement capture-avoiding subs
-  e1 :$ e2 -> do
-    f <- eval env e1
-    e <- eval env e2
-    apply f e 
-  where
-    apply v x = case v of
-      Fun f -> f x
+-- reduce = \case
+--   e1 :$ e2 -> \case
+    
+
+
+
+-- -- | Primitive operations
+-- data OP = Plus | Times deriving (Eq, Show)
+
+-- -- | Expression type
+-- data Expr v a =
+--     v :-> Expr v a       -- ^ Lambda
+--   | Expr v a :$ Expr v a -- ^ Apply
+--   -- | Prim OP (Expr v a) (Expr v a) -- ^ Primitive operation (e.g. '+')
+--   | Const a
+--   deriving (Eq, Show)
+
+-- -- | 'Let' is equivalent to applying a lambda
+-- let_ :: v -> Expr v a -> Expr v a -> Expr v a
+-- let_ v e body = (v :-> body ) :$ e
+
+-- lets_ :: Foldable t => t (v, Expr v a) -> Expr v a -> Expr v a
+-- lets_ decls body = foldl (flip $ uncurry let_) body decls
+
+
+-- -- | Value type
+-- data Value m a =
+--     Val a
+--   | Fun (Value m a -> m (Value m a))
+
+-- -- -- | Interpreting an 'Expr' produces a 'Value'
+-- -- interp env = \case
+-- --   Const x -> pure $ Val x
+-- --   v :-> body -> pure $ Fun $ \v -> interp env' body  -- implement capture-avoiding subs
+-- --   e1 :$ e2 -> do
+-- --     f <- interp env e1
+-- --     e <- interp env e2
+-- --     apply f e 
+-- --   where
+-- --     apply v x = case v of
+-- --       Fun f -> f x
 
 
 
