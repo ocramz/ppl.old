f027eef77ed8eff789989d7e06d4f9261001163f
ocramz <zocca.marco@gmail.com>
2018-10-28 14:42:55 +0100

    impl Internal.Mini

--- src/PPL/Internal/Mini.hs
+++ src/PPL/Internal/Mini.hs
@@ -1,10 +1,19 @@
+{-# language LambdaCase #-}
 module PPL.Internal.Mini where
 
+-- | Value type
+data Value a =
+    Val a
+  | Fun (Value a -> Value a)
 
+-- | Primitive operations
+data OP = Plus | Times deriving (Eq, Show)
 
+-- | Expression type
 data Expr v a =
-    v :-> Expr v a       -- Lambda
-  | Expr v a :$ Expr v a -- Apply
+    v :-> Expr v a       -- ^ Lambda
+  | Expr v a :$ Expr v a -- ^ Apply
+  -- | Prim OP (Expr v a) (Expr v a) -- ^ Primitive operation (e.g. '+')
   | Const a
   deriving (Eq, Show)
 
@@ -14,3 +23,8 @@ let_ v e body = (v :-> body ) :$ e
 
 lets_ :: Foldable t => t (v, Expr v a) -> Expr v a -> Expr v a
 lets_ decls body = foldl (flip $ uncurry let_) body decls
+
+-- | Evaluating an 'Expr' produces a 'Value'
+eval = \case
+  Const x -> Val x
+  v :-> body -> undefined
