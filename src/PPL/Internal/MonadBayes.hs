4482e9327c67cf1ea23f70ed5704f19dd0887848
ocramz <zocca.marco@gmail.com>
2018-11-13 21:38:57 +0100

    add MWC.Prob instance

--- src/PPL/Internal/MonadBayes.hs
+++ src/PPL/Internal/MonadBayes.hs
@@ -1,7 +1,10 @@
 {-# language DeriveFunctor, GeneralizedNewtypeDeriving #-}
+{-# language RankNTypes #-}
 module PPL.Internal.MonadBayes where
 
 import Numeric.Log
+
+import Control.Monad.Primitive
 
 -- import Data.Number.LogFloat
 import qualified Control.Monad.Trans.State as S
@@ -17,13 +20,18 @@ class Monad m => MonadSample m where
   bernoulli :: R -> m Bool
   bernoulli p = fmap (< p) uniform
   normal :: R -> R -> m R
+  -- normal mu sig = MWC.normal mu sig
+
+instance PrimMonad m => MonadSample (MWC.Prob m) where
+  uniform = MWC.uniform
+  normal = MWC.normal 
 
 class Monad m => MonadCond m where
   score :: Log R -> m ()
 
 class (MonadSample m, MonadCond m) => MonadInfer m
 
-
+-- | Weighting inference transformer
 newtype W m a = W (S.StateT (Log R) m a) deriving (Functor, Applicative, Monad)
 
 instance T.MonadTrans W where
@@ -37,3 +45,10 @@ mkW fs = W (S.state fs)
 
 runW :: W m a -> m (a, Log R)
 runW (W w) = S.runStateT w 1
+
+instance Monad m => MonadCond (W m) where
+  score w = W (S.modify (* w))
+
+-- | Lift a natural transformation into a W
+hoistW :: (forall x . m x -> n x) -> W m a -> W n a
+hoistW f (W m) = W $ S.mapStateT f m
