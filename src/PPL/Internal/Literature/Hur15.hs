a1a53e6227d0976a6adb692ad8300ad9b59fb358
ocramz <zocca.marco@gmail.com>
2018-11-15 22:25:00 +0100

    add notes from Hur et al., 2015

--- src/PPL/Internal/Literature/Hur15.hs
+++ src/PPL/Internal/Literature/Hur15.hs
@@ -1,25 +1,44 @@
 {-# language DeriveFunctor, GeneralizedNewtypeDeriving #-}
+{-# language MultiParamTypeClasses, TypeFamilies #-}
 module PPL.Internal.Literature.Hur15 where
 
 import qualified Control.Monad.Trans.State as S
 import qualified Data.Map.Strict as M
-
+import Numeric.Log
 import Prelude hiding (lookup)
 
 -- | Univariate continuous distributions
 data Dist1 a =
     Normal a a
-  | Uniform01
+  | Uniform a a
   deriving (Eq, Show)
 
-newtype Theta k d = Theta (M.Map k [(d, Dist1 d)]) deriving (Eq, Show)
+newtype Theta k d1 d = Theta (M.Map k [(d1, Dist1 d)]) deriving (Eq, Show)
 
-lookup k (Theta mm) = M.lookup k mm
+-- empty :: Theta k d
+-- empty = Theta M.empty
+
+-- lookup :: Ord k => k -> Theta k d -> Maybe [(d, Dist1 d)]
+-- lookup k (Theta mm) = M.lookup k mm
+
+-- append :: Ord k => k -> (d, Dist1 d) -> Theta k d -> Theta k d
+append k v (Theta mm) = Theta $ M.insertWith (++) k [v] mm
 
 
-newtype HistT k d m a = HistT {
-  unHistT :: S.StateT (Theta k d) m a
+
+newtype HistT k d1 d m a = HistT {
+  unHistT :: S.StateT (Theta k d1 d) m a
   } deriving (Functor, Applicative, Monad)
+
+class Observe m where
+  observe :: String -> (Double, Dist1 Double) -> m ()
+--   -- type D m :: *
+--   observe :: String -> (D m, Dist1 (D m)) -> m ()
+
+instance Monad m => Observe (HistT k d1 d m) where
+--   -- type D (HistT d m) = d
+  -- observe k v = HistT $ S.modify (append k v)  
+  
 
 
 
